deec69bb30b4165f9664aad8d50f42bea24a00aa
aFAVIER <anthony.favier03@gmail.com>
2017-03-08 11:05:22 +0100

    exo4_1

---
+++ function.php
@@ -0,0 +1,102 @@
+<?php
+	//exo1
+	function exo1(){
+		echo 'true';
+	};
+	//exo2
+	
+	//exo3
+	
+ 	//exo4
+ 	
+ 	//exo5
+ 	
+ 	//exo6
+ 	
+ 	//exo7
+ 	
+ 	//exo8
+ 	
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title></title>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="style/css/bootstrap.css">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+				<!--Mon fichier css pour quelques modif-->
+	<link rel="stylesheet" type="text/css" href="style/css/style.css">
+</head>
+<body>
+	<header></header>
+	<main class="container">
+ 		<h5>
+ 		<?php
+			//exo1
+			exo1();
+ 		?>
+ 		</h5>
+ 		<h5>
+ 		<?php
+			//exo2
+			
+ 		?>
+ 		</h5>
+ 		<h5>
+ 		<?php
+			//exo3
+ 			
+	 			
+ 		?>
+ 		</h5>
+ 		<h5>
+ 		<?php
+			//exo4
+
+ 		?>
+ 		</h5>
+ 		<h5>
+ 		<?php
+			//exo5
+	 		?>
+ 		</h5>
+ 		<h5>
+ 		<?php
+			//exo6
+ 		
+ 		?>
+ 		</h5>
+ 		<h5>
+ 		<?php
+			//exo7
+ 			
+ 		?>
+ 		</h5>
+ 		<h5>
+ 		<?php
+			//exo8
+ 			
+ 		?>
+ 		</h5>
+	
+	</main>
+	<script type="text/javascript" src='js/jquery-3.1.1.slim.js'></script>
+	<script type="text/javascript" src='js/parallax.min.js'></script>
+	<script type="text/javascript" src='js/bootstrap.min.js'></script>
+	<script type="text/javascript" src='js/script.js'></script>
+</body>
+</html>
